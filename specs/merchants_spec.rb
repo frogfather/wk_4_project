e96568f9913967229955dfb157c2e18f899d8df9
John Campbell <j.a.campbell@icloud.com>
2016-11-24 16:57:25 +0000

    added method delete_all to merchant

--- specs/merchants_spec.rb
+++ specs/merchants_spec.rb
@@ -6,20 +6,29 @@ require_relative('../models/merchants')
 class TestMerchant < Minitest::Test
 
   def setup
+  Merchant.delete_all
   @merchant1 = Merchant.new({"name" => "Sainsbury"})
+  @merchant2 = Merchant.new({"name" => "Tesco"})
   end
+
+  
 
   def test_merchant_name
     assert_equal('Sainsbury', @merchant1.name)
   end
 
-  def test_merchant_id
+  #def test_merchant_id
     #only works on if id = 1
     #run cashbox.sql first!
-    @merchant1.save
-    assert_equal(1, @merchant1.id)
+  #  @merchant1.save
+   # assert_equal(1, @merchant1.id)
+  #end
+
+  def test_select_all
+  @merchant1.save 
+  @merchant2.save
+  merchants = Merchant.all
   end
-  
-  
+
 
 end
