57f28f96062df54b601c458105db8ab14d003dec
John Campbell <j.a.campbell@icloud.com>
2016-11-24 16:41:49 +0000

    added method save to merchants. Also wrote a spec for it

--- specs/merchants_spec.rb
+++ specs/merchants_spec.rb
@@ -4,3 +4,22 @@ require('pry-byebug')
 require_relative('../models/merchants')
 
 class TestMerchant < Minitest::Test
+
+  def setup
+  @merchant1 = Merchant.new({"name" => "Sainsbury"})
+  end
+
+  def test_merchant_name
+    assert_equal('Sainsbury', @merchant1.name)
+  end
+
+  def test_merchant_id
+    #only works on if id = 1
+    #run cashbox.sql first!
+    @merchant1.save
+    assert_equal(1, @merchant1.id)
+  end
+  
+  
+
+end
