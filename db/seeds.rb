9dd77f235e58168692d81028999848754d4433f2
John Campbell <j.a.campbell@icloud.com>
2016-11-25 15:14:22 +0000

    added rather a lot of stuff - views, category and merchant methods to transaction class and such

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,18 @@ Category.delete_all
 
 merchant1 = Merchant.new({"name" => "Sainsburys"})
 merchant1.save
+merchant2 = Merchant.new({"name" => "Tesco"})
+merchant2.save
+merchant3 = Merchant.new({"name" => "Lidl"})
+merchant3.save
+
 category1 = Category.new({"name" => "Food"})
 category1.save
+category2 = Category.new({"name" => "Wine"})
+category2.save
+category3 = Category.new({"name" => "Clothes"})
+category3.save
+
 transaction1 = Transaction.new({"trans_date" => "11/25/2016", "amount" => "23.43", "merchant_id" => merchant1.id, "category_id" => category1.id})
-binding.pry
 transaction1.save
-id = transaction1.id
-Transaction.delete(id)
+
